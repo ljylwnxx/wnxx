1fbe27c63e4dd0c9cb13f98edfdfefa91d1ee2ca
ljylwnxx <ljylwnxx@163.com>
2023-04-25 13:51:19 +0800

    feat:layout

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    222
+  <div class="frame">
+    <aside>aside</aside>
+    <div class="main-container">
+      <div class="fixed-header">
+        <navbar>111</navbar>
+      </div>
+      <app-main>222</app-main>
+    </div>
   </div>
 </template>
 
@@ -8,5 +14,23 @@
 </script>
 
 <style scoped>
-
+.frame{
+  /* width: 100vw; */
+  /* height: 100vh; */
+  display: flex;
+  flex-direction: column;
+  border: solid 10px #00151f;
+  border-radius: 20px;
+  background-color: #f4f5f7;
+  transition: all 0.4s ease-in-out;
+}
+.aside {
+  height: 80px;
+}
+.main {
+  height: 100%;
+}
+.footer {
+  height: 80px;
+}
 </style>
