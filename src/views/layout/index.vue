df42065b38d23569a805f19d5aff6832fbc4c868
ljylwnxx <ljylwnxx@163.com>
2023-04-26 18:37:39 +0800

    style:设置背景半透明

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -15,13 +15,14 @@
 
 <style scoped>
 .frame{
-  /* width: 100vw; */
-  /* height: 100vh; */
+  width: 100vw;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   border: solid 10px #00151f;
   border-radius: 20px;
-  background-color: #f4f5f7;
+  background-color: rgba(255, 255, 255, 0.5);
+  backdrop-filter: blur(50px);
   transition: all 0.4s ease-in-out;
 }
 .aside {
