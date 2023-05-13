d21e79a8c1422b77a095e92e83c767c5ab535163
ljylwnxx <ljylwnxx@163.com>
2023-05-13 21:23:15 +0800

    revert:md测试

--- src/views/layout/components/AppMain.vue
+++ src/views/layout/components/AppMain.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="app-main">
     333
-    <md></md>
   </div>
 </template>
 
 <script setup lang="ts">
-import md from '../../../pages/test.md'
 import { useRouter } from 'vue-router'
 const router = useRouter()
 </script>
