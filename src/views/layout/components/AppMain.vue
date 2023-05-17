cb1b9c17d3dcdcd753de573ac808dbe7ff2a715e
ljylwnxx <ljylwnxx@163.com>
2023-05-17 16:54:06 +0800

    revert:删除新建

--- src/views/layout/components/AppMain.vue
+++ src/views/layout/components/AppMain.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="app-main">
+    <test></test>
     <Applist></Applist>
   </div>
 </template>
 
 <script setup lang="ts">
+import test from '@/pages/test.md'
 </script>
 
 <style scoped>
